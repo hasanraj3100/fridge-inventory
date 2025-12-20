252e75b6640ce2cc1917e74b49d9ec544c117e54
Raj <hasanraj3100@gmail.com>
2025-12-21 01:48:12 +0600

    Fix user domain missing updated_at

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -9,4 +9,5 @@ type User struct {
 	Email        string    `json:"email" db:"email"`
 	PasswordHash string    `json:"password_hash" db:"password_hash"`
 	CreatedAt    time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
 }
